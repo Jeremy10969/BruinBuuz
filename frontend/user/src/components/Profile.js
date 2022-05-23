3a5eb6ab5f7d1e78e33930cd80a65844de25fd2b
Yanjianan Zhu <zhuyanjordan@163.com>
2022-05-22 23:15:24 -0700

    change the gallery of profile page to grids

--- frontend/user/src/components/Profile.js
+++ frontend/user/src/components/Profile.js
@@ -26,6 +26,8 @@ const Profile = ()=>{
                 <img className="post" src="https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg"/>
                 <img className="post" src="https://navsci.ucla.edu/wp-content/uploads/2020/04/bruin-bound.jpg"/>
                 <img className="post" src="https://cdn.dribbble.com/users/559871/screenshots/15470728/media/9e081b71dfe6dec27a37e8c9bfc1af35.png?compress=1&resize=400x300"/>
+		<img className="post" src="https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg"/>
+		<img className="post" src="https://static8.depositphotos.com/1377527/943/i/600/depositphotos_9431737-stock-photo-portrait-of-gray-striped-cat.jpg"/>
             </div>
         </div>
     )
