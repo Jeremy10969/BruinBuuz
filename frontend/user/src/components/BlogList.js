3509ab70fb54ee9a779d6250245bf3e0ddf09075
hercysn <hercyshen@gmail.com>
2022-05-26 00:32:41 -0700

    Fixed the displaying only texts bug.

--- frontend/user/src/components/BlogList.js
+++ frontend/user/src/components/BlogList.js
@@ -5,10 +5,12 @@ const BlogList = ({ blogs }) => {
                 <div className="feed"  key={blog._id}>
                     <h5>{ blog.title }</h5>
                     <p>Written by ...</p>
+                        {blog.picture !== '' && blog.picture !== 'no pic' &&
                         <div className="feed-image">
                         <img style={{width:"100%", height:"70%", objectFit:"cover"}} 
                         src={blog.picture} />
                         </div>
+                        }
                 <div className="feed-content"  >
                     <i className="material-icons">favorite_border</i>
                     <h6>Tag TB Implemented</h6>
