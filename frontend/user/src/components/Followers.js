cb61b0c83f4cf75d62fb47bc726b4de62e88578d
hercysn <hercyshen@gmail.com>
2022-06-03 17:19:52 -0700

    Fix always removing first follower problem

--- frontend/user/src/components/Followers.js
+++ frontend/user/src/components/Followers.js
@@ -10,6 +10,7 @@ const Followers = () => {
     const [followersBlogList, setFollowersBlogList] = useState([]);
     const [removeFollower, setRemoveFollower] = useState(null);
     const location = useLocation();
+
     useEffect(() => {
         if (removeFollower){
             fetch("http://"+window.location.host.split(":")[0]+":4000/removefollower/" + removeFollower,
@@ -26,8 +27,11 @@ const Followers = () => {
                 }
             )
         }
-        
+
     }, [removeFollower])
+
+ 
+
     useEffect(() => {
         setFollowersBlogList([]);
         userInfo.followers.map( (followersuser, index) => {
@@ -65,33 +69,25 @@ const Followers = () => {
         {
             followersBlogList.length!=0?
             followersBlogList.map(
-                (obj, index)=>{
-                    return(
-                        <div className='follower-list'>
+                (obj)=>(
+                        
+                        <div className='follower-list' key={obj.user._id}>
                             <h5>
                                 <img className="profile-name" src={obj.user.picture} />
                                 {
                                     <a className="profile-username" href={"/users/"+obj.user.username}>{obj.user.username}</a>
                                 }
-                                {location.pathname.substring(0,5)=='/user'?"":<label for="remove-follower"><i className='material-icons'>remove_circle_outline</i></label>}
-                            
-                                <button id="remove-follower" onClick={()=>{setRemoveFollower(obj.user._id)}} hidden/>                           
+                                {location.pathname.substring(0,5)=='/user'?"":<label for="remove-follower"><i className='material-icons' onClick={()=>{setRemoveFollower(obj.user._id)}}>remove_circle_outline</i></label>}
+                                                    
                             </h5>
                             <PostEntranceList posts={obj.blogs.slice(0,3)}/>
                         </div>
-                    )                   
-                }
+                                   
+                )
             )    
             :
             <h5>No followers currently.</h5>     
         }
-
-        
-       
-
-       
-
-       
     </div>
 
     )
